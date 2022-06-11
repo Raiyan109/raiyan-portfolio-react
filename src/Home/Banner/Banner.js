a28e953b087d75e06a304c4e617623cc3925116d
Raiyan109 <kraiyan109@gmail.com>
2022-06-11 23:56:03 +0600

    new banner

--- src/Home/Banner/Banner.js
+++ src/Home/Banner/Banner.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import styles from './Banner.module.css'
-import videoBg from '../../assets/videos/videoBg.mp4'
+
 
 const Banner = () => {
     return (
-        <div className={styles.hero}>
-            <div className={styles.overlay}></div>
-            <video src={videoBg} autoPlay loop muted></video>
-            <div className={styles.heroContent}>
+        <div >
 
-                <div className={styles.heroTitle}>
-                    <h1>
-                        Raiyan Kabir
-                    </h1>
-                    <h3>Web Developer</h3>
-                    <button className={styles.heroButton}>Download Resume</button>
-                </div>
-                <h1 className={styles.heroSubtitle}>
-
-                </h1>
-
-
-            </div>
         </div>
     );
 };
