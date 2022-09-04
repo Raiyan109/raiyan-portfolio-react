826c34c359ca4ce2915079ae32604f70b116d2e3
Raiyan109 <kraiyan109@gmail.com>
2022-09-04 07:59:56 +0600

    experience card inner style done

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -10,16 +10,85 @@ const Experience = () => {
             <h1 className="text-3xl font-bold mt-10 mb-10">What skills I have</h1>
             <h1 className="text-5xl font-bold mt-10 mb-10">My Experiences</h1>
 
-            <div className={styles.frontendContainer}>
-                <h1 className='flex justify-center items-center pt-5 font-bold text-2xl'>Frontend Development</h1>
+            <div className='grid grid-cols-2 gap-4'>
+                <div className={styles.frontendContainer}>
+                    <h1 className='flex justify-center items-center pt-5 font-bold text-2xl'>Frontend Development</h1>
 
-                <div className={styles.frontendContent}>
-                    <article className={styles.frontendDetails}>
-                        <span>
-                            <FontAwesomeIcon icon={faCheckCircle} color='golden' />
-                        </span>
+                    <div className={styles.frontendContent}>
+                        <article className={styles.frontendDetails}>
+                            <FontAwesomeIcon className={styles.experienceFontAwesome} icon={faCheckCircle} color='golden' />
+                            <div>
+                                <h4 className='text-2xl'>HTML</h4>
+                                <small className='font-thin'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className={styles.frontendDetails}>
+                            <FontAwesomeIcon className={styles.experienceFontAwesome} icon={faCheckCircle} color='golden' />
+                            <div>
+                                <h4 className='text-2xl'>CSS</h4>
+                                <small className='font-thin'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className={styles.frontendDetails}>
+                            <FontAwesomeIcon className={styles.experienceFontAwesome} icon={faCheckCircle} color='golden' />
+                            <div>
+                                <h4 className='text-2xl'>JavaScript</h4>
+                                <small className='font-thin'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className={styles.frontendDetails}>
+                            <FontAwesomeIcon className={styles.experienceFontAwesome} icon={faCheckCircle} color='golden' />
+                            <div>
+                                <h4 className='text-2xl'>Bootstrap</h4>
+                                <small className='font-thin'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className={styles.frontendDetails}>
+                            <FontAwesomeIcon className={styles.experienceFontAwesome} icon={faCheckCircle} color='golden' />
+                            <div>
+                                <h4 className='text-2xl'>Tailwind</h4>
+                                <small className='font-thin'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className={styles.frontendDetails}>
+                            <FontAwesomeIcon className={styles.experienceFontAwesome} icon={faCheckCircle} color='golden' />
+                            <div>
+                                <h4 className='text-2xl'>React</h4>
+                                <small className='font-thin'>Experienced</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
 
-                    </article>
+                {/* END OF FRONTEND */}
+
+                <div className={styles.backendContainer}>
+                    <h1 className='flex justify-center items-center pt-5 font-bold text-2xl'>Backend Development</h1>
+
+                    <div className={styles.frontendContent}>
+                        <article className={styles.frontendDetails}>
+                            <FontAwesomeIcon className={styles.experienceFontAwesome} icon={faCheckCircle} color='golden' />
+                            <div>
+                                <h4 className='text-2xl'>NodeJS</h4>
+                                <small className='font-thin'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className={styles.frontendDetails}>
+                            <FontAwesomeIcon className={styles.experienceFontAwesome} icon={faCheckCircle} color='golden' />
+                            <div>
+                                <h4 className='text-2xl'>ExpressJS</h4>
+                                <small className='font-thin'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className={styles.frontendDetails}>
+                            <FontAwesomeIcon className={styles.experienceFontAwesome} icon={faCheckCircle} color='golden' />
+                            <div>
+                                <h4 className='text-2xl'>MongoDB</h4>
+                                <small className='font-thin'>Experienced</small>
+                            </div>
+                        </article>
+
+                    </div>
                 </div>
             </div>
         </div>
